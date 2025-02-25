a98d21660cb0215d2178e7f2b5a82dd18135a4a4
emalico <emalicohii@gmail.com>
2025-02-25 08:59:17 +0100

    Add files via upload

---
+++ programim.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+ /*cout "hello world";
+   return 0;*/
+   
+ /*cout eshte character output dhe perdoret ne mesazhe
+ endl eshte per kalimin e rreshtit*/
+ cout<<"*******  **  **   **"<<endl;
+ cout<<"****     * *  *   ****"<<endl;
+ cout<<"*****    *    *  *   *"<<endl;
+ return 0;
+ 
+ 
+}  
+
+
